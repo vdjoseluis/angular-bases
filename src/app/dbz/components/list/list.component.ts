ca2cfd9b2a7e0480abc205ded736fb8ff74f6f6a
Jose Vasquez <vdjoseluis@outlook.com>
2024-11-12 11:17:48 +0100

    Agregar personaje a la lista DBZ

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-dbz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  @Input()    // Se puede poner dentro de () algun nombre
+  public characterList: Character[] = [{ name: 'Trunks', power: 10 }];  //valor por default si se pasa vacio
 
+  onDeleteCharacter(index: number): void {
+    //TODO: Emitir el ID del personaje
+    console.log({index})
+  }
 }
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
@@ -11,6 +12,7 @@ import { AddCharacterComponent } from './components/add-character/add-character.
   exports: [MainPageComponent],
   imports: [
     CommonModule,
+    FormsModule
   ]
 })
 export class DbzModule { }
